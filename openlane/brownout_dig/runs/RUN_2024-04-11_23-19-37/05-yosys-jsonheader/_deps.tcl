b0a88dbda8bd82adb8342b6b7e5bb1fd6e409ccc
Robin Matthew Tsang <robin.tsang@ajacci.com>
2024-04-15 13:27:36 -0500

    documentation

---
+++ openlane/brownout_dig/runs/RUN_2024-04-11_23-19-37/05-yosys-jsonheader/_deps.tcl
@@ -0,0 +1,5 @@
+set ::_synlig_defines [list]
+verilog_defines -DUSE_POWER_PINS
+lappend ::_synlig_defines +define+USE_POWER_PINS
+read_liberty -lib -ignore_miss_dir -setattr blackbox /home/rtsang/.volare/volare/sky130/versions/bdc9412b3e468c102d01b7cf6337be06ec6e9c9a/sky130A/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
+set ::env(SYNTH_LIBS) /home/rtsang/chipalooza/sky130_ajc_ip__brownout/openlane/brownout_dig/runs/RUN_2024-04-11_23-19-37/tmp/cf69170abd3b458a95bee2083ab8035e.lib
